5d5cebe39b059f55fc06116d19816aae8ffcf9ca
mlau154 <mlauer2015@gmail.com>
2022-10-25 09:42:34 -0500

    Added api.rst and conf.py

---
+++ docs/source/conf.py
@@ -0,0 +1,22 @@
+# Project Information
+
+project = 'pymead'
+copyright = '2022, Matthew G. Lauer'
+author = 'Matthew G. Lauer'
+
+release = '2.0'
+version = '2.0.0'
+
+extensions = [
+    'sphinx.ext.autosummary',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.doctest',
+    'sphinx.ext.intersphinx',
+]
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+
+html_theme = 'sphinx_rtd_theme'
