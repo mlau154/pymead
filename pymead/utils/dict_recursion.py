a26ba7620a475c9592e4cad7862908472266fda4
mlauer154 <mlauer2@illinois.edu>
2022-12-06 22:50:51 -0600

    Added methods for param_dict serialization in GUI and moved 'decimals' key in dialog setInputs

--- pymead/utils/dict_recursion.py
+++ pymead/utils/dict_recursion.py
@@ -1,5 +1,7 @@
 from functools import reduce
 
+import numpy as np
+
 
 def set_all_dict_values(d: dict):
     for k, v in d.items():
@@ -30,3 +32,20 @@ def assign_names_to_params_in_param_dict(d: dict, name_str: str = ''):
 def recursive_get(d, *keys):
     """From answer by Thomas Orozco (https://stackoverflow.com/a/28225747)"""
     return reduce(lambda c, k: c.get(k, {}), keys, d)
+
+
+def unravel_param_dict(d: dict, output_dict: dict, prep_for_json: bool = True):
+    for k, v in d.items():
+        if isinstance(v, dict):
+            output_dict[k] = {}
+            unravel_param_dict(v, output_dict[k], prep_for_json=prep_for_json)
+        else:
+            param_dict_attrs = vars(v)
+            for attr_key, attr in param_dict_attrs.items():
+                if prep_for_json:
+                    if isinstance(attr, np.ndarray):
+                        param_dict_attrs[attr_key] = attr.tolist()
+                    if attr_key in ['mea', 'param_dict', 'free_point', 'anchor_point', 'affects', 'depends_on',
+                                    'function_dict', 'func']:
+                        param_dict_attrs[attr_key] = None
+            output_dict[k] = param_dict_attrs
