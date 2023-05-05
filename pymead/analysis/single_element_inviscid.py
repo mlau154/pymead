735a39ade4c757e99d33a8b46fb46cafdeb0f438
mlau154 <mlauer2015@gmail.com>
2023-05-04 20:13:08 -0500

    Updated docs

--- pymead/analysis/single_element_inviscid.py
+++ pymead/analysis/single_element_inviscid.py
@@ -11,6 +11,31 @@ from numba import jit
 
 @jit(nopython=True, cache=True)
 def single_element_inviscid(coord: np.ndarray, alpha: float or np.float64):
+    r"""
+    A linear strength vortex panel method for the inviscid solution of a single airfoil, sped up using the
+    just-in-time compiler in *numba*. Directly adapted from "Program 7" of Appendix D in [1].
+
+    [1] J. Katz and and A. Plotkin, Low-Speed Aerodynamics, Second Edition, 2nd ed. New York, NY,
+    USA: Cambridge University Press, 2004. Accessed: Mar. 07, 2023. [Online].
+    Available: `<https://asmedigitalcollection.asme.org/fluidsengineering/article/126/2/293/458666/LowSpeed-Aerodynamics-Second-Edition>`_
+
+    Parameters
+    ==========
+    coord: np.ndarray
+      An :math:`N \times 2` array of airfoil coordinates, where :math:`N` is the number of coordinates, and the columns
+      represent :math:`x` and :math:`y`
+
+    alpha: float or np.float64
+      Angle of attack of the airfoil
+
+    Returns
+    =======
+    np.ndarray, np.ndarray, float
+      The first returned array is of size :math:`(N-1) \times 2` and represents the :math:`x` and :math:`y` locations
+      of the collocation points, where :math:`N` is the number of airfoil coordinates. The second returned array is a
+      one-dimensional array with length :math:`(N-1)` representing the surface pressure coefficient at each collocation
+      point. The final returned value is the lift coefficient.
+    """
 
     N = len(coord)          # Number of panel end points
     M = N - 1                 # Number of control points
