1fd872ce3e9c6b811f414d53cb1d9d7c322bad48
mlau154 <mlauer2015@gmail.com>
2023-05-04 20:13:08 -0500

    Updated docs

--- pymead/analysis/cfd_output_templates.py
+++ pymead/analysis/cfd_output_templates.py
@@ -1,3 +1,8 @@
+"""
+Templates to use for aerodynamic forces output of XFOIL and MSES
+"""
+
+
 XFOIL = {'Cd': 0.01, 'Cl': 0.5, 'alf': 0.0, 'Cm': 0.01, 'Cdf': 0.005, 'Cdp': 0.005, 'L/D': 50.0, 'Cp': {}}
 MSES = {'Cd': 0.01, 'Cl': 0.5, 'alf': 0.0, 'Cm': 0.01, 'Cdf': 0.005, 'Cdp': 0.005, 'Cdw': 0.005,
         'Cdv': 0.005, 'Cdh': 0.0, 'L/D': 50.0, 'BL': []}
