7588763e5d97552af3ea4a517f5ad2805d0dbfd6
mlau154 <mlauer2015@gmail.com>
2023-05-19 15:33:54 -0500

    Updated Docs - BaseAirfoilParams

--- pymead/core/base_airfoil_params.py
+++ pymead/core/base_airfoil_params.py
@@ -1,96 +1,116 @@
+import typing
+
 from pymead.core.param import Param
 from pymead.core.pos_param import PosParam
 from pymead.core.anchor_point import AnchorPoint
 from pymead.core.trailing_edge_point import TrailingEdgePoint
-import numpy as np
 
 
 class BaseAirfoilParams:
 
     def __init__(self,
                  airfoil_tag: str = None,
-                 c: Param = None,  # chord length
-                 alf: Param = None,  # angle of attack (rad)
-                 R_le: Param = None,  # leading edge radius
-                 L_le: Param = None,  # leading edge length
-                 r_le: Param = None,  # leading edge length ratio
-                 phi_le: Param = None,  # leading edge 'tilt' angle
-                 psi1_le: Param = None,  # leading edge upper curvature control angle
-                 psi2_le: Param = None,  # leading edge lower curvature control angle
-                 L1_te: Param = None,  # trailing edge upper length
-                 L2_te: Param = None,  # trailing edge lower length
-                 theta1_te: Param = None,  # trailing edge upper angle
-                 theta2_te: Param = None,  # trailing edge lower angle
-                 t_te: Param = None,  # blunt trailing edge thickness
-                 r_te: Param = None,  # blunt trailing edge thickness length ratio
-                 phi_te: Param = None,  # blunt trailing edge 'tilt' angle
-                 dx: Param = None,  # dx to translate
-                 dy: Param = None,  # dy to translate
-                 non_dim_by_chord: bool = False,
+                 c: Param = None,
+                 alf: Param = None,
+                 R_le: Param = None,
+                 L_le: Param = None,
+                 r_le: Param = None,
+                 phi_le: Param = None,
+                 psi1_le: Param = None,
+                 psi2_le: Param = None,
+                 L1_te: Param = None,
+                 L2_te: Param = None,
+                 theta1_te: Param = None,
+                 theta2_te: Param = None,
+                 t_te: Param = None,
+                 r_te: Param = None,
+                 phi_te: Param = None,
+                 dx: Param = None,
+                 dy: Param = None,
                  ):
-        """
-        ### Description:
-
-        The most fundamental parameters required for the generation of any `pymead.core.airfoil.Airfoil`.
-        A geometric description of an example airfoil generated (from `pymead.examples.simple_airfoil.run()`) is
+        r"""
+        The most fundamental parameters required for the generation of any ``pymead.core.airfoil.Airfoil``.
+        A geometric description of an example airfoil generated (from ``pymead.examples.simple_airfoil.run()``) is
         shown below (it may be helpful to open the image in a new tab to adequately view the details):
 
-        ### Args:
+        Parameters
+        ==========
+        airfoil_tag: str
+          The Airfoil to which this set of base parameters belongs.
 
-        `c`: ( \\(c\\) ) chord length `pymead.core.param.Param`: Default value: `1.0`.
+        c: Param or None
+          :math:`c`: Chord length. Default value if ``None``: ``Param(1.0)``.
 
-        `alf`: ( \\(\\alpha\\) ) angle of attack (rad): `pymead.core.param.Param`. Default value: `0.0`.
+        alf: Param or None
+          :math:`\alpha`: Angle of attack [rad]. Default value if ``None``: ``Param(0.0)``.
 
-        `R_LE`: ( \\(R_{LE}\\) ) leading-edge radius: `pymead.core.param.Param`. Default value: `0.1`.
+        R_le: Param or None
+          :math:`R_{LE}`: Leading-edge radius. Default value if ``None``: ``Param(0.1)``.
 
-        `L_LE`: ( \\(L_{LE}\\) ) distance between the control points immediately before and after the leading-edge
-        anchor point: `pymead.core.param.Param`. Default value: `0.1`.
+        L_le: Param or None
+          :math:`L_{LE}`: Distance between the control points immediately before and after the leading-edge
+          anchor point. Default value if ``None``: ``Param(0.1)``.
 
-        `r_LE`: ( \\(r_{LE}\\) ) ratio of the distance from the leading-edge anchor point to the control point before to
-        the distance between the control points immediately before and after the leading-edge
-        anchor point ( \\( r_{LE} = L_{LE,upper} / L_{LE} \\) ): `pymead.core.param.Param`. Default value: `0.5`.
+        r_le: Param or None
+          :math:`r_{LE}`: Ratio of the distance from the leading-edge anchor point to the control point before to
+          the distance between the control points immediately before and after the leading-edge
+          anchor point (:math:`r_{LE} = L_{LE,\text{upper}} / L_{LE}`). Default value if ``None``: ``Param(0.5)``.
 
-        `phi_LE`: ( \\(\\phi_{LE}\\) ) leading-edge tilt (rad), referenced counter-clockwise from the perpendicular to
-        the chordline: `pymead.core.param.Param`. Valid range: \\( \\phi_{LE} \\in [-180^{\\circ},180^{\\circ}]\\).
-        Default value: `0.0`.
+        phi_le: Param or None
+          :math:`\phi_{LE}`: Leading-edge tilt (rad), referenced counter-clockwise from the perpendicular to
+          the chordline. Default value if ``None``: ``Param(0.0)``.
 
-        `psi1_LE`: ( \\(\\psi_{LE,1}\\) ) leading-edge upper curvature control angle (rad), referenced counter-clockwise
-        from the chordline: `pymead.core.param.Param`. Valid range: \\( \\psi_{1,LE} \\in [-90^{\\circ},90^{\\circ}] \\).
-        Default value: `0.0`.
+        psi1_le: Param or None
+          :math:`\psi_{LE,1}`: Leading-edge upper curvature control angle (rad), referenced counter-clockwise
+          from the chordline: Default value if ``None``: ``Param(0.0)``.
 
-        `psi2_LE`: ( \\(\\psi_{LE,2}\\) ) leading-edge lower curvature control angle (rad), referenced clockwise
-        from the chordline: `pymead.core.param.Param`. Valid range: \\( \\psi_{2,LE} \\in [-90^{\\circ},90^{\\circ}] \\).
-         Default value: `0.0`.
+        psi2_le: Param or None
+          :math:`\psi_{LE,2}`: Leading-edge lower curvature control angle (rad), referenced clockwise
+          from the chordline. Default value if ``None``: ``Param(0.0)``.
 
-        `L1_TE`: ( \\(L_{TE,1}\\) ) trailing edge upper length: `pymead.core.param.Param`. Default value: `0.1`.
+        L1_te: Param or None
+          :math:`L_{TE,1}`: Trailing edge upper length. Default value if ``None``: ``Param(0.1)``.
 
-        `L2_TE`: ( \\(L_{TE,2}\\) ) trailing edge lower length: `pymead.core.param.Param`. Default value: `0.1`.
+        L2_te: Param or None
+          :math:`L_{TE,2}`: Trailing edge lower length. Default value if ``None``: ``Param(0.1)``.
 
-        `theta1_TE`: ( \\(\\theta_{TE,1}\\) ) trailing edge upper angle (rad), referenced clockwise from the chordline:
-        `pymead.core.param.Param`. Default value: `np.deg2rad(10.0)`.
+        theta1_te: Param or None
+          :math:`\theta_{TE,1}`: Trailing edge upper angle (rad), referenced clockwise from the chordline.
+          Default value if ``None``: ``Param(np.deg2rad(10.0))``.
 
-        `theta2_TE`: ( \\(\\theta_{TE,2}\\) ) trailing edge lower angle (rad), referenced counter-clockwise from the
-        chordline: `pymead.core.param.Param`. Default value: `np.deg2rad(10.0)`.
+        theta2_te: Param or None
+          :math:`\theta_{TE,2}`: Trailing edge lower angle (rad), referenced counter-clockwise from the
+          chordline. Default value if ``None``: ``Param(np.deg2rad(10.0))``.
 
-        `t_TE`: ( \\( t_{TE}\\) ) blunt trailing edge thickness: `pymead.core.param.Param`. Default value: `0.0`.
+        t_te: Param or None
+          :math:`t_{TE}`: Blunt trailing edge thickness.
+          Default value if ``None``: ``Param(0.0)`` (sharp trailing edge).
 
-        `r_TE`: ( \\(r_{TE}\\) ) ratio of the distance from the chordline's endpoint at the trailing edge to the
-        first control point of the airfoil to the distance between the first and last control points of the airfoil
-        ( \\( r_{TE} = L_{TE,upper} / L_{TE} \\) ): `pymead.core.param.Param`. Default value: `0.5`.
+        r_te: Param or None
+          :math:`r_{TE}`: Ratio of the distance from the chordline's endpoint at the trailing edge to the
+          first control point of the airfoil to the distance between the first and last control points of the airfoil
+          (:math:`r_{TE} = L_{TE,upper} / L_{TE}`). This parameter has no effect
+          on the airfoil geometry unless ``t_te != Param(0.0)``. Default value if ``None``: ``Param(0.5)``.
 
-        `phi_TE`: ( \\( \\phi_{TE}\\) ) blunt trailing-edge tilt (rad), referenced counter-clockwise from the
-        perpendicular to the chordline (same as `phi_LE`): `pymead.core.param.Param`. Default value: `0.0`.
+        phi_te: Param or None
+          :math:`\phi_{TE}`: Blunt trailing-edge tilt (rad), referenced counter-clockwise from the
+          perpendicular to the chordline (same as ``phi_le``). This parameter has no effect
+          on the airfoil geometry unless ``t_te != Param(0.0)``. Default value if ``None``: ``Param(0.0)``.
 
-        `dx`: ( \\( \\Delta x\\) ) distance to translate the airfoil in the \\(x\\)-direction:
-        `pymead.core.param.Param`. Default value: `0.0`. The translation operation follows the rotation operation
-        such that the rotation operation can be performed about the origin. Inactive by default.
+        dx: Param or None
+          :math:`\Delta x`: Distance to translate the airfoil in the :math:`x`-direction. The translation operation
+          follows the rotation operation such that the rotation operation can be performed about the origin.
+          Default value if ``None``: ``Param(0.0)``.
 
-        `dy`: ( \\( \\Delta y\\) ) distance to translate the airfoil in the \\(y\\)-direction:
-        `pymead.core.param.Param`. Default value: `0.0`. The translation operation follows the rotation operation
-        such that the rotation operation can be performed about the origin. Inactive by default.
+        dy: Param or None
+          :math:`\Delta y`: Distance to translate the airfoil in the :math:`y`-direction. The translation operation
+          follows the rotation operation such that the rotation operation can be performed about the origin.
+          Default value if ``None``: ``Param(0.0)``.
 
-        `non_dim_by_chord`: a `bool` that determines whether the `pymead.core.param.Param`s with `units == length`
-        should be non-dimensionalized by the chord (`c`). Default value: `True`.
+        Returns
+        =======
+        BaseAirfoilParams
+          An instance of the ``BaseAirfoilParams`` class.
         """
         self.airfoil_tag = airfoil_tag
         self.c = c
@@ -146,19 +166,14 @@ class BaseAirfoilParams:
         if not self.dy:
             self.dy = Param(0.0)
 
-        # if not -np.pi <= self.phi_le.value <= np.pi:
-        #     raise ValueError(f'The leading edge tilt angle, phi_le, must be between -180 degrees and 180 degrees,'
-        #                      f'inclusive. A value of {phi_le.value} was entered.')
-        #
-        # if not -np.pi / 2 <= self.psi1_le.value <= np.pi / 2:
-        #     raise ValueError(f'The upper curvature control arm angle, psi1_le, must be between -90 degrees and '
-        #                      f'90 degrees, inclusive. A value of {psi1_le.value} was entered.')
-        #
-        # if not -np.pi / 2 <= self.psi2_le.value <= np.pi / 2:
-        #     raise ValueError(f'The lower curvature control arm angle, psi2_le, must be between -90 degrees and '
-        #                      f'90 degrees, inclusive. A value of {psi2_le.value} was entered.')
+    def generate_main_anchor_points(self) -> typing.List[AnchorPoint]:
+        """
+        Generates the minimal set of ``pymead.core.anchor_point.AnchorPoint``\ s required for an Airfoil in pymead.
 
-    def generate_main_anchor_points(self):
+        Returns
+        =======
+        typing.List[AnchorPoint]
+        """
         le_anchor_point = AnchorPoint(PosParam((0.0, 0.0)), 'le', 'te_1', self.airfoil_tag, self.L_le, self.R_le,
                                       self.r_le, self.phi_le, self.psi1_le, self.psi2_le)
         te_1_anchor_point = TrailingEdgePoint(self.c, self.r_te, self.t_te, self.phi_te, self.L1_te, self.theta1_te,
