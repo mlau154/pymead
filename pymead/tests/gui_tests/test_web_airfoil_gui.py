9153d526e74392114f7b86b9d5b7cfcde2e7a01f
mlauer154 <mlauer2@illinois.edu>
2024-06-10 14:43:06 -0500

    Added web-airfoil generation GUI tests

---
+++ pymead/tests/gui_tests/test_web_airfoil_gui.py
@@ -0,0 +1,43 @@
+import unittest
+import sys
+
+from PyQt5.QtWidgets import QApplication, QDialog
+
+from gui_tests.utils import perform_action_on_dialog
+from pymead.gui.dialogs import WebAirfoilDialog
+
+from pymead.gui.gui import GUI
+
+app = QApplication(sys.argv)
+app.processEvents()
+app.setStyle('Fusion')
+
+
+class WebAirfoilGUITest(unittest.TestCase):
+    def setUp(self) -> None:
+        """Create the GUI"""
+        if len(sys.argv) > 1:
+            self.gui = GUI(path=sys.argv[1])
+        else:
+            self.gui = GUI()
+
+    def test_load_n0012(self):
+
+        def dialog_action(dialog: QDialog):
+            dialog.accept()
+
+        perform_action_on_dialog(self.gui.main_icon_toolbar.buttons["generate-web-airfoil"]["button"].click,
+                                 dialog_action)
+        polyline_subcontainer = self.gui.geo_col.container()["polylines"]
+        self.assertTrue("n0012-1" in polyline_subcontainer)
+
+    def test_load_sc20612(self):
+
+        def dialog_action(dialog: WebAirfoilDialog):
+            dialog.inputs[1].setValue("sc20612-il")
+            dialog.accept()
+
+        perform_action_on_dialog(self.gui.main_icon_toolbar.buttons["generate-web-airfoil"]["button"].click,
+                                 dialog_action)
+        polyline_subcontainer = self.gui.geo_col.container()["polylines"]
+        self.assertTrue("sc20612-1" in polyline_subcontainer)
---
+++ pymead/tests/gui_tests/utils.py
@@ -0,0 +1,58 @@
+import time
+import typing
+
+from PyQt5.QtCore import QTimer
+from PyQt5.QtWidgets import QDialog, QApplication
+
+
+def perform_action_on_dialog(dialog_trigger: typing.Callable,
+                             on_dialog_open_action: typing.Callable,
+                             time_out: int = 5) -> QDialog:
+    """
+    Solution for getting active dialog for GUI testing. From
+    https://github.com/pytest-dev/pytest-qt/issues/256#issuecomment-1915675942
+
+    Returns the current dialog (active modal widget). If there is no
+    dialog, it waits until one is created for a maximum of 5 seconds (by
+    default).
+
+    Parameters
+    ----------
+    dialog_trigger: typing.Callable
+        Function that triggers the dialog creation.
+
+    on_dialog_open_action: typing.Callable
+        Function that must have a QDialog as its sole argument. This function is called immediately after the
+        dialog is found.
+
+    time_out: int
+        Maximum time (seconds) to wait for the dialog creation.
+    """
+
+    dialog: QDialog = None
+    start_time = time.time()
+
+    # Helper function to catch the dialog instance and hide it
+    def dialog_creation():
+        # Wait for the dialog to be created or timeout
+        nonlocal dialog
+        while dialog is None and time.time() - start_time < time_out:
+            dialog = QApplication.activeModalWidget()
+
+        # Avoid errors when dialog is not created
+        if dialog is not None:
+            on_dialog_open_action(dialog)
+
+    # Create a thread to get the dialog instance and call dialog_creation trigger
+    QTimer.singleShot(1, dialog_creation)
+    dialog_trigger()
+
+    # Wait for the dialog to be created or timeout
+    while dialog is None and time.time() - start_time < time_out:
+        continue
+
+    assert isinstance(
+        dialog, QDialog
+    ), f"No dialog was created after {time_out} seconds. Dialog type: {type(dialog)}"
+
+    return dialog
