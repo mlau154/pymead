e17099fcd44133053faba50976566932313dd08a
aparua2 <aparua2@illinois.edu>
2024-07-20 00:56:15 -0500

    test_load_examples

--- pymead/tests/gui_tests/test_menu_items.py
+++ pymead/tests/gui_tests/test_menu_items.py
@@ -1,14 +1,21 @@
+from pymead.gui.gui import GUI
 from pymead.tests.gui_tests.utils import app
+from pymead.utils.read_write_files import load_data
 
 
 def test_load_examples(app):
-
-    example_dict = {}
+    gui = GUI
+    menu_json_file = r"../../gui/gui_settings/menu.json"
+    menu_dict = load_data(menu_json_file)
+    load_example_data = menu_dict["File"]["Load Example"]
 
     def _test_load_example_recursively(d: dict):
         for k, v in d.items():
-            pass
+            if isinstance(v, dict):
+                _test_load_example_recursively(v)
 
-    _test_load_example_recursively(example_dict)
+            else:
+                assert hasattr(gui, v)
 
-    # assert len(app.geo_col.container()["airfoils"]) != 0
+    _test_load_example_recursively(load_example_data)
+
