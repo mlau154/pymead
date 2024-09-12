e213172ae26f5ac92b7156959c8651af2bff86b1
mlau154 <mlauer2015@gmail.com>
2024-09-12 11:22:21 -0500

    Edit Bounds table now resizes to contents on initialization

--- pymead/gui/bounds_values_table.py
+++ pymead/gui/bounds_values_table.py
@@ -15,8 +15,7 @@ class BoundsValuesTable(TableWidget):
         self.setHorizontalHeaderLabels(["DesVar Name", "Lower Bound", "Value", "Upper Bound"])
         for column in (0, 2):
             self.makeColumnReadOnly(column)
-        for column in range(self.columnCount()):
-            self.setColumnWidth(column, 100)
+        self.resizeColumnsToContents()
         self.initializing_data = False
 
     def handleItemChanged(self, item):
