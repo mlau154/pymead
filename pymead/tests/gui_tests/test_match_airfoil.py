07733ffcbb0ca1ccbdf056e57cf1ad8b0cac9b53
aparua2 <aparua2@illinois.edu>
2024-11-06 17:26:26 -0600

    test_match_airfoil timer implimentation

--- pymead/tests/gui_tests/test_match_airfoil.py
+++ pymead/tests/gui_tests/test_match_airfoil.py
@@ -1,8 +1,12 @@
 from pymead.tests.gui_tests.utils import app
 
+import shutil
+import time
+import warnings
+
 
 def test_match_airfoil(app):
-    airfoil = app.load_example_basic_airfoil_blunt()
+    app.load_example_basic_airfoil_blunt_dv()
     params_container = app.geo_col.container()["params"]
     params_container["Length-6"].v = 0.00126
     params_container["Length-7"].v = 0.00126
@@ -15,3 +19,20 @@ def test_match_airfoil(app):
 
     app.match_airfoil(dialog_test_action=dialog_action)
 
+    t_increment, max_time = 0.1, 60.0
+    t = 0.0
+
+    start_time = time.time()
+    while (time.time() - start_time) < max_time:
+        if len(app.text_area.toPlainText()) > 1:
+            text_box = app.text_area.toPlainText()
+            print(text_box)
+            break
+        else:
+            time.sleep(t_increment)
+
+    if len(app.text_area.toPlainText()) == 0:
+        print("Test timed out")
+
+    #how do hit enter second time after test runs (get rid of info box)
+    #not correctly entering if block (not correctly getting text from console?)
