8db58bd27097e3435523b760e7fb41a9fd8438af
Matt Lauer <mlauer2015@gmail.com>
2024-11-25 10:17:27 -0600

    Added norm kwargs to generate_field_matplotlib

--- pymead/post/mses_field.py
+++ pymead/post/mses_field.py
@@ -23,6 +23,7 @@ def generate_field_matplotlib(axs: plt.Axes or None,
                               analysis_subdir: str,
                               var: str,
                               cmap_field: mpl_colors.Colormap or str,
+                              norm=None,
                               vmin: float = None,
                               vmax: float = None,
                               arrow_start_J_idx: int = 20,
@@ -85,7 +86,7 @@ def generate_field_matplotlib(axs: plt.Axes or None,
         flow_var_section = flow_var[:, start_idx:end_idx]
 
         args = (x_grid[flow_section_idx], y_grid[flow_section_idx], flow_var_section)
-        kwargs = dict(cmap=cmap_field, vmin=vmin, vmax=vmax)
+        kwargs = dict(cmap=cmap_field, vmin=vmin, vmax=vmax, norm=norm)
 
         if axs is None:
             pcolormesh_handles[f'field_{flow_section_idx}'] = plt.pcolormesh(*args, **kwargs)
